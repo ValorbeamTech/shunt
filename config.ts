842b79a6a390a47cf77a89c65787f8ca4794c0bf
Valorbeam Tech <valorbeam.info@gmail.com>
2023-12-08 13:01:16 +0300

    initial server setup

---
+++ config.ts
@@ -0,0 +1,2 @@
+export const hostname = 'localhost';
+export const port = 1000
---
+++ server.ts
@@ -0,0 +1,27 @@
+// import dependencies 
+import http from 'http'
+import { port, hostname } from './config'
+
+// define a middleware function
+function logInformation(_req: http.IncomingMessage, _res: http.ServerResponse, next: () => void) {
+    console.log('Hello, World!');
+    next(); // Calling next to proceed with the next middleware or route handler
+}
+
+
+// create a server instance
+const app = http.createServer((_req: http.IncomingMessage, res: http.ServerResponse)=>{
+    res.setHeader('Content-Type', 'application/json')
+    res.statusCode = 200
+    res.end("Hello, world!")
+
+})
+
+// adding middleware to a server
+app.on('request', (req: http.IncomingMessage, res: http.ServerResponse)=>{
+    logInformation(req, res, ()=>{
+    })
+})
+
+// listening to a server port
+app.listen(port, ()=> console.log(`App is running on host http://${hostname} and listening on port ${port}`))
