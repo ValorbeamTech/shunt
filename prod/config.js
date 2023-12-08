a917d8b10b8c6aaee3d32230b551dbe0712d58c2
Valorbeam Tech <valorbeam.info@gmail.com>
2023-12-08 13:07:29 +0300

    Initial server setup and dependencies installation

---
+++ prod/config.js
@@ -0,0 +1,5 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+exports.port = exports.hostname = void 0;
+exports.hostname = 'localhost';
+exports.port = 1000;
---
+++ prod/server.js
@@ -0,0 +1,21 @@
+"use strict";
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
+Object.defineProperty(exports, "__esModule", { value: true });
+const http_1 = __importDefault(require("http"));
+const config_1 = require("./config");
+function logInformation(_req, _res, next) {
+    console.log('Hello, World!');
+    next();
+}
+const app = http_1.default.createServer((_req, res) => {
+    res.setHeader('Content-Type', 'application/json');
+    res.statusCode = 200;
+    res.end("Hello, world!");
+});
+app.on('request', (req, res) => {
+    logInformation(req, res, () => {
+    });
+});
+app.listen(config_1.port, () => console.log(`App is running on host http://${config_1.hostname} and listening on port ${config_1.port}`));
