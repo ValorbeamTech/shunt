c1957d4d3aaa3a39734602ef3fe19edc8aa9e70b
Valorbeam Tech <valorbeam.info@gmail.com>
2023-12-10 16:27:55 +0300

    remove customs property in interfaces

--- config/httpInterface.ts
+++ config/httpInterface.ts
@@ -2,8 +2,6 @@
 import * as http from 'http'
 
 export interface HttpRequest extends http.IncomingMessage {
-    customProperty?: string;
 }
 export interface HttpResponse extends http.ServerResponse {
-    customMethod?(): void;
 }
